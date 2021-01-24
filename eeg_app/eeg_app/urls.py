bebf0818f3e3869ae1dcc0ffd37daa87d9c095f0
Oskar <67687200+GitOskar@users.noreply.github.com>
2021-01-24 14:00:35 +0100

    All frontend changes

--- eeg_app/eeg_app/urls.py
+++ eeg_app/eeg_app/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from eeg_app.views import index
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
